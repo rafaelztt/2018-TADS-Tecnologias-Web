3d2adbc2c906d25835bfe6bc81401ce73850743c
Aluno <rafael.zottesso@ifpr.edu.br>
2018-11-05 21:42:13 -0200

    Exemplo de php para incluir arquivos/conteúdos externos.

---
+++ 3_trimestre/aula32.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 32: Criando sites com PHP</title>
+  </head>
+  <body>
+
+    <?php include("menu.php"); ?>
+
+    <h2>Exemplo de site no PHP</h2>
+    <p>
+      Você pode colocar todo seu HTML dentro
+      deste arquivo PHP que vai funcionar da
+      mesma forma.
+    </p>
+    <p>
+      Para acessar seu site, vá em
+      http://localhost/seu_repositorio/seu_arquivo.php
+    </p>
+
+
+
+  </body>
+</html>
---
+++ 3_trimestre/menu.php
@@ -0,0 +1,12 @@
+<!--
+Aqui vai somente o código do seu menu!
+
+NADA MAIS!
+-->
+<ul>
+  <li><a href="#">Página inicial</a></li>
+  <li><a href="#">Sobre</a></li>
+  <li><a href="#">Contato</a></li>
+  <li><a href="#">Cadastro</a></li>
+  <li><a href="#">Login</a></li>
+</ul>
